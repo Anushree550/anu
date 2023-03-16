61b10329fedadb95d92c593d5c8c372cb389de65
Anushree550 <125344090+Anushree550@users.noreply.github.com>
2023-03-16 21:26:53 +0530

    del

--- Module _4 - Advance python programming/11).Write_list_to_file.py
+++
@@ -1,4 +0,0 @@
-items = ['Mango', 'Orange', 'Apple', 'Lemon']
-file = open('C:\\Users\\gohil\\OneDrive\\Documents\\GitHub\\Jay_Tops-Work\\Python_Language\\Assignments\\Module _4 - Advance python programming\\11)items.txt','w')
-file.writelines(items)
-file.close()
